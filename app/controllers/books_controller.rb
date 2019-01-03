e3dfe94c8007a1810d0a4c8d169cf19783a75d3b
Adrian Messado <adrian.messado@gmail.com>
2019-01-03 13:35:42 -0800

    add comment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
 		@books = Book.all
 	end
 	def show
+		#retrieve a single book, and save it in @book. Then we retrieve all of it's reviews and save them in @reviews. The has_many / belongs_to association lets us query for reviews like this
 		@book = Book.find(params[:id])
 		@reviews = @book.reviews
 	end
