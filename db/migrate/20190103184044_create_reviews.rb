a7a52f9670782366174293295c505f6578dd6602
Adrian Messado <adrian.messado@gmail.com>
2019-01-03 11:07:06 -0800

    add references to book model

--- db/migrate/20190103184044_create_reviews.rb
+++ db/migrate/20190103184044_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.string :author
       t.string :comment
+      t.references :book
       t.timestamps
     end
   end
