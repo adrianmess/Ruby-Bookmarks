b78600c65d446a39bbb82cabc1bd0abf42c57681
Adrian Messado <adrian.messado@gmail.com>
2019-01-03 10:59:37 -0800

    add columns to reviews table

--- db/migrate/20190103184044_create_reviews.rb
+++ db/migrate/20190103184044_create_reviews.rb
@@ -1,7 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.string :author
+      t.string :comment
       t.timestamps
     end
   end
